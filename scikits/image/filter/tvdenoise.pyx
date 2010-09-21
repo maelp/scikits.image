b3b5398934f853ffe7be91ad2c9e128d9c185c9d
Mael Primet <mael.primet@gmail.com>
2010-09-21 12:14:12 +0200

    Removed tvdenoise_float64, renamed tvdenoise_float32 to tvdenoise, added a docstring to the tvdenoise algorithm

--- scikits/image/filter/tvdenoise.pyx
+++ scikits/image/filter/tvdenoise.pyx
@@ -3,9 +3,8 @@ cimport numpy as np
 
 cdef extern from "c_src/c_tvdenoise.c":
   int c_tvdenoise_float(float *, int, int, int, float, float *)
-  int c_tvdenoise_double(double *, int, int, int, double, double *)
 
-def tvdenoise_float32(arr, int n=10, float W=50.0):
+def tvdenoise(arr, int niter=10, float W=50.0):
   """
   Perform total-variation denoising on a float32 array
 
@@ -14,11 +13,25 @@ def tvdenoise_float32(arr, int n=10, float W=50.0):
   arr: ndarray
        input data to be denoised
 
-  n: int, optional
+  niter: int, optional
      number of iterations
 
   W: float, optional
      denoising weight
+
+  Returns
+  -------
+  out : ndarray
+    The denoised image
+  
+  Examples
+  --------
+  >>> import os
+  >>> from scikits.image import data_dir
+  >>> from scikits.image.io import imread
+
+  >>> lena = imread(os.path.join(data_dir, 'lena256.tif'))
+  >>> lena_denoised = tvdenoise(lena, niter=5)
   """
   if len(arr.shape) != 2:
     raise ValueError('Expecting a 2d array')
@@ -33,41 +46,8 @@ def tvdenoise_float32(arr, int n=10, float W=50.0):
       contarr = flat_arr.astype(np.float32)
   cdef np.ndarray[np.float32_t] out = np.zeros_like(contarr)
 
-  err_code = c_tvdenoise_float(<float *>contarr.data, nx, ny, n, W, <float *>out.data)
-  if err_code != 0:
-    raise RuntimeError('c_tvdenoise exited with code '+str(err_code))
-
-  return out.reshape((nx, ny))
-
-def tvdenoise_float64(arr, int n=10, double W=50.0):
-  """
-  Perform total-variation denoising on a float64 array
-
-  Parameters
-  ----------
-  arr: ndarray
-       input data to be denoised
-
-  n: int, optional
-     number of iterations
-
-  W: float, optional
-     denoising weight
-  """
-  if len(arr.shape) != 2:
-    raise ValueError('Expecting a 2d array')
-  cdef int nx = arr.shape[0]
-  cdef int ny = arr.shape[1]
-
-  cdef np.ndarray[np.float64_t] flat_arr = arr.ravel()
-  cdef np.ndarray[np.float64_t, mode='c'] contarr
-  try:
-      contarr = flat_arr
-  except:
-      contarr = flat_arr.astype(np.float64)
-  cdef np.ndarray[np.float64_t] out = np.zeros_like(contarr)
-
-  err_code = c_tvdenoise_double(<double *>contarr.data, nx, ny, n, W, <double *>out.data)
+  err_code = c_tvdenoise_float(<float *>contarr.data, nx, ny,
+      niter, W, <float *>out.data)
   if err_code != 0:
     raise RuntimeError('c_tvdenoise exited with code '+str(err_code))
 
